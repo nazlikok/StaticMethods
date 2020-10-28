c67784437aeb91d37ae591054b2ade6e8f782fac
nazlikok <nazlikok@hotmail.com>
2020-10-28 23:00:06 +0300

    4.th

--- src/com/company/Assignment04.java
+++ src/com/company/Assignment04.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 public class Assignment04 {
 
     public static void main(String[] args) {
+
         System.out.println("Give me a number");
         Scanner num1 = new Scanner(System.in);
         int input = (int) num1.nextFloat();
-
         //isEven(input);
         System.out.println(input+ " is even " +"-> "+ isEven(input));
-
-        /*
-        System.out.println("What is the area code that you'r looking for?");
-        Scanner sc = new Scanner(System.in);
-        String codeInput = sc.nextLine();
-        String st = resolveAreaCode(codeInput);
-         */
 
         System.out.println("What is the area code that you'r looking for?");
         Scanner num2 = new Scanner(System.in);
         int inputCode = (int) num2.nextFloat();
         resolveAreaCode(inputCode);
 
+        System.out.println("Give me a number to test fibonacci");
+        Scanner num3 = new Scanner(System.in);
+        int fibonacci = (int) num3.nextFloat();
+        fibonacciSeries(fibonacci);
+
+        System.out.println("Give me a number to test Maxfibonacci");
+        Scanner num4 = new Scanner(System.in);
+        int max = (int) num4.nextFloat();
+        fibonacciUntil(max);
+
     }
+
     static boolean isEven(int testnum) {
         if (testnum % 2 == 0) {
             return true;
@@ -31,6 +35,7 @@ public class Assignment04 {
             return false;
         }
     }
+
     static String resolveAreaCode(int inputCode){
         switch (inputCode){
             case 212:
@@ -72,4 +77,31 @@ public class Assignment04 {
         return null;
     }
 
+    static void fibonacciSeries(int n){
+        int previous = 0;
+        int add = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            System.out.print(previous + " + ");
+            int sum = previous + add;
+            previous = add;
+            add = sum;
+        }
+        System.out.println();
+    }
+
+    static void fibonacciUntil(int max){
+        int m1 = 0;
+        int m2 = 1;
+        for (int i = 1; i <=max ; i++) {
+            System.out.print(m1 + " + ");
+            int sum = m1 + m2;
+            m1 = m2;
+            m2 = sum;
+            if (max < m1) {
+                break;
+            }
+
+        }
+    }
 }
